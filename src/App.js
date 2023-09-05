e74a6fb0a8e20d927ad19a2dfaf209ac02b738c4
GarvonGit <chouhan.garv100@gmail.com>
2023-09-05 13:47:55 +0530

    last commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import UseState from "./hooks/useState";
+//import UseEffect from "./hooks/useEffect";
+import UseReducer from "./hooks/useReducer";
+
+const App = () => {
+  return <UseReducer />;
+};
+
+export default App;
